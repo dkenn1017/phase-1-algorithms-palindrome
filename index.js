3d11205896d717d2b94d7902d95998faaa8dd7aa
darrylkennedy <darrylkennedy13@gmail.com>
2024-01-15 19:07:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+function reverse(word) {
+ //Write your algorithm here
+  return word.split("").reverse().join("");
+}
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  that means if the word is the same as the word in reverse, I should return true.
+  
+  reverse the input string
+
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  set constant variable for a palendrome, then write a function for a palendrome which returns for it.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +35,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
